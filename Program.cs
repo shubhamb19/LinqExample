3234500c129e9f4eb0f4db347d604e92ba115ee5
shubhamb19 <shubhambhuse19@gmail.com>
2024-10-14 20:44:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LinqEg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] numbers = { 1, 7, 3, 13, 5, 6 };
+            // var oddNumbers = numbers.Where(n => (n % 2) == 1);
+            // Console.WriteLine(String.Join(",", oddNumbers));
+            List<string> stringList = new List<string>() 
+            {
+                    "C# Tutorials",
+                    "VB.NET Tutorials",
+                    "Learn C++",
+                    "MVC Tutorials" ,
+                    "harsha"
+            };
+
+            var result = stringList.Where(s => s.Contains("harsha"));
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
